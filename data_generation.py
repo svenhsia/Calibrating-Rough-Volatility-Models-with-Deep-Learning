bd5ded7cbec236b4794cc5e47660d7ade52ef532
svenhsia <svenhsia@gmail.com>
2019-04-10 19:07:03 +0200

    complete CNN-calibration notebook

---
+++ data_generation.py
@@ -0,0 +1,19 @@
+"""This module generates synthetic labled data for:
+1. Heston model
+2. rBergomi model
+"""
+
+
+def generate_data(n_samples, model='heston'):
+    """
+    Parameters:
+    -----------
+        n_samples: integer, number of samples to generate
+        model: str, the name of the model, 'heston' or 'rbergomi'
+
+    Returns:
+    --------
+        X: input data
+        Y: lables
+    """
+    pass
---
+++ neural_network.py
@@ -0,0 +1,8 @@
+"""This module use generated data to build Neural Network for prediction.
+"""
+
+import Keras as K
+
+
+def feature_scaling(X_train, scaling=''):
+    pass
