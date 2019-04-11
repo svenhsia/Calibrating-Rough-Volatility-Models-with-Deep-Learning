cb0227071819a4b227677cfd9117242cb301b74e
svenhsia <svenhsia@gmail.com>
2019-04-12 00:59:25 +0200

    add data generation

---
+++ rbergomi/utils.py
@@ -0,0 +1,65 @@
+import numpy as np
+from scipy.stats import norm
+from scipy.optimize import brentq
+
+def g(x, a):
+    """
+    TBSS kernel applicable to the rBergomi variance process.
+    """
+    return x**a
+
+def b(k, a):
+    """
+    Optimal discretisation of TBSS process for minimising hybrid scheme error.
+    """
+    return ((k**(a+1)-(k-1)**(a+1))/(a+1))**(1/a)
+
+def cov(a, n):
+    """
+    Covariance matrix for given alpha and n, assuming kappa = 1 for
+    tractability.
+    """
+    cov = np.array([[0.,0.],[0.,0.]])
+    cov[0,0] = 1./n
+    cov[0,1] = 1./((1.*a+1) * n**(1.*a+1))
+    cov[1,1] = 1./((2.*a+1) * n**(2.*a+1))
+    cov[1,0] = cov[0,1]
+    return cov
+
+def bs(F, K, V, o = 'call'):
+    """
+    Returns the Black call price for given forward, strike and integrated
+    variance.
+    """
+    # Set appropriate weight for option token o
+    w = 1
+    if o == 'put':
+        w = -1
+    elif o == 'otm':
+        w = 2 * (K > 1.0) - 1
+
+    sv = np.sqrt(V)
+    d1 = np.log(F/K) / sv + 0.5 * sv
+    d2 = d1 - sv
+    P = w * F * norm.cdf(w * d1) - w * K * norm.cdf(w * d2)
+    return P
+
+def bsinv(P, F, K, t, o = 'call'):
+    """
+    Returns implied Black vol from given call price, forward, strike and time
+    to maturity.
+    """
+    # Set appropriate weight for option token o
+    w = 1
+    if o == 'put':
+        w = -1
+    elif o == 'otm':
+        w = 2 * (K > 1.0) - 1
+
+    # Ensure at least instrinsic value
+    P = np.maximum(P, np.maximum(w * (F - K), 0))
+
+    def error(s):
+        return bs(F, K, s**2 * t, o) - P
+    s = brentq(error, 1e-9, 1e+9)
+    return s
